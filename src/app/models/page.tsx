ec88c9c8b2475bc2f85b9863f9ae621f26d4f9eb
Siratee Topradit <tae.siratee@gmail.com>
2025-03-21 11:32:05 +0700

    feat: implement models list page with links to car models

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -1,8 +1,24 @@
-export default function Model() {
-    return (
-      <div>
-        Model
+"use client";
+
+import { CAR_MODELS } from "@/data/carModels";
+import Link from "next/link";
+
+export default function ModelsListPage() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <div className="container mx-auto px-4 py-12">
+
+        <div className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {Object.values(CAR_MODELS).map((car) => (
+            <Link 
+              href={`/models/${car.id}`} 
+              key={car.id}
+              >
+              {car.name}
+            </Link>
+          ))}
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
