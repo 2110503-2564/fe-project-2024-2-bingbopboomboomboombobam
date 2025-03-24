38914e1f77b5a5298a4dd423f00f7a496630ffdd
Nattapol Teerayuttawong <nyutwong@gmail.com>
2025-03-24 20:40:13 +0700

    fix: vercel error

--- src/app/mybookings/page.tsx
+++ src/app/mybookings/page.tsx
@@ -1,9 +1,6 @@
-// import { headers } from 'next/headers';
-// import { useEffect } from 'react';
-
 import CarCard from "@/components/CarCard";
 
-export default function MyBookings({ searchParams }: { searchParams: { id: string } }) {
+export default function MyBookings() {
   return (
     <div className="p-20 pt-30 flex flex-row w-full justify-center flex-wrap gap-5">
       <CarCard carname="Jaguar XJL 2016" price="$96.50" seat="2 PEOPLE" transmission="7-speed ISR" driveTrain="AWS" />
